bc193f3de843381540934bcec3483472277dc126
Erik Persson <npersson001@gmail.com>
2017-07-07 16:15:10 -0400

    actually updated the multithread

--- Thread/MultiThread.h
+++ Thread/MultiThread.h
@@ -5,29 +5,27 @@
 #pragma once
 
 #include "Thread.h"
-#include "JsonBox.h"
 #include <vector>
+#include <map>
 
 namespace atl
 {
 
-/**
-* \brief Multithread class that calls on Thread class functions
-**/
 class MultiThread : public Thread
 {
 public:
-    MultiThread(int numThreads = 2): m_numThreads(numThreads) {}	//!< Number of threads
+    MultiThread(int numThreads=2): m_numThreads(numThreads) {}
     virtual ~MultiThread();
-    virtual bool setNumThreads(unsigned numThreads);
+    virtual bool setNumThreads(unsigned);
     virtual bool Start(bool* runFlag = nullptr);
     virtual bool Join();
     virtual bool Detach();
+
+protected:
     virtual int getMyId();
 
-private:
-    unsigned                    m_numThreads;   //!< Number of threads to spawn
-    std::vector<std::thread>    m_threads;      //!< Running threads
-    std::map<std::thread::id,int> m_idMap; 		//<! map of Ids
+    unsigned                    m_numThreads;   //<! Number of threads to spawn
+    std::vector<std::thread>    m_threads;      //<! Running threads
+    std::map<std::thread::id,int>  m_idMap;      //<! map of Ids
 };
 }
