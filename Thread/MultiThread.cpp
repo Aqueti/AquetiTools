a99639c20bb626dbdb3231473f3088d819633c3a
Erik Persson <npersson001@gmail.com>
2017-07-07 15:36:57 -0400

    merge stuff still

--- Thread/MultiThread.cpp
+++ Thread/MultiThread.cpp
@@ -1,13 +1,9 @@
 /**
- * \file MultiThread.cpp
- **/
-
+ * \fiel MultiThread.cpp
+ */
 #include "MultiThread.h"
 #include <iostream>
 #include <fstream>
-#include <system_error>
-
-std::ofstream multiThreadTest;
 
 namespace atl
 {
@@ -33,21 +29,20 @@ MultiThread::~MultiThread()
  */
 bool MultiThread::Start(bool* runFlag)
 {
-    if(m_running && *m_running) {
-        std::cerr << "WARNING: Threads already running." << std::endl;
+    if (m_running && *m_running) {
+        std::cout << "WARNING: Threads already running." << std::endl;
         return false;
     }
 
-    if(!runFlag) {
+    if (!runFlag) {
         m_deletePtr = true;
         m_running = new bool(true);
     } else {
         m_running = runFlag;
     }
 
-    for(unsigned i = 0; i < m_numThreads; i++) {
-        m_threads.emplace_back([this] {Execute();});
-        m_idMap.emplace(m_threads.crbegin()->get_id(), i);
+    for (unsigned i = 0; i < m_numThreads; i++) {
+        m_threads.emplace(m_threads.end(), [this] {Execute();});
     }
     return true;
 }
@@ -59,67 +54,25 @@ bool MultiThread::Start(bool* runFlag)
  */
 bool MultiThread::Join()
 {
-    std::unique_lock<std::mutex> guard (m_threadMutex);
-    auto deleteThreads = std::move(m_threads);
-    guard.unlock();
 
     bool rc = true;
-    for(auto&& t: deleteThreads) {
-        try {
-            t.join();
-        } catch (const std::system_error& e) {
-            if(e.code() == std::errc::resource_deadlock_would_occur) {
-                std::cerr << "Warning: Cant join yourself! detaching..." << std::endl;
-                try {
-                    t.detach();
-                } catch (const std::system_error& e) {
-                    std::cerr << "Warning: Join could not detach: " << e.what() << std::endl;
-                    rc = false;
-                }
-            } else if(e.code() == std::errc::no_such_process) {
-                std::cerr << "Warning: Thread not valid: " << e.what() << std::endl;
-                rc = false;
-            } else if(e.code() == std::errc::invalid_argument) {
-                std::cerr << "Warning: Thread not joinable: " << e.what() << std::endl;
-                std::cerr << "t.id: " << t.get_id() << std::endl;
-                std::cerr << "me.id: " << std::this_thread::get_id() << std::endl;
-                std::cerr << "joinable: " << t.joinable() << std::endl;
-                rc = false;
-            } else {
-                std::cerr << "Warning: Unknown thread error occurred: " << e.what() << std::endl;
-                rc = false;
-            }
+    for (auto&& t: m_threads) {
+        if (!t.joinable() || t.get_id() == std::this_thread::get_id()) {
+            std::cout << "WARNING: Thread id: " << t.get_id() << " not joinable" << std::endl;
+            rc = false;
+            continue;
         }
+        t.join();
     }
 
-    guard.lock();
-    if(m_deletePtr && m_running) {
+    std::lock_guard<std::mutex> guard (m_threadMutex);
+    m_threads.clear();
+
+    if (m_deletePtr && m_running) {
         delete m_running;
         m_running = nullptr;
     }
     m_deletePtr = false;
-
-    m_idMap.clear();
-
-    return rc;
-}
-
-/**
- * @brief Waits for all threads to complete before returning
- *
- * @return true if all threads were able to join
- */
-bool MultiThread::Detach()
-{
-    bool rc = true;
-    for(auto&& t: m_threads) {
-        try {
-            t.detach();
-        } catch (const std::system_error& e) {
-            std::cerr << "Warning: Detach could not detach: " << e.what() << std::endl;
-            rc = false;
-        }
-    }
 
     return rc;
 }
@@ -172,20 +125,4 @@ bool MultiThread::setNumThreads(unsigned numThreads)
     m_numThreads = numThreads;
     return true;
 }
-
-/**
- * @brief Returns a thread ID for this thread.  Will be an int between 0 and n-1,
- * where n is the number of threads
- *
- * @return -1 on failure, or an int between 0 and n-1
- */
-int MultiThread::getMyId()
-{
-    if(!m_idMap.count(std::this_thread::get_id())) {
-        std::cerr << "ERROR: called getMyId from thread outside of MultiThread" << std::endl;
-        return -1;
-    }
-    return m_idMap[std::this_thread::get_id()];
-}
-
 }
