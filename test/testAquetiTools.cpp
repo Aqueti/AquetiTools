952e46ced91f5d0fe03c29953d947320173a2413
Steve Feller <sfeller@aqueti.com>
2017-09-05 11:15:49 -0400

    Fixed printout

--- test/testAquetiTools.cpp
+++ test/testAquetiTools.cpp
@@ -16,11 +16,12 @@ bool printHelp(void)
     std::cout << std::endl;
     std::cout << "Usage: ./testAquetiTools <options>" << std::endl;
     std::cout << "Options: " << std::endl;
-    std::cout << "\t-h    prints this help menu and exits" << std::endl;
-    std::cout << "\t-t    is followed by name of method to be tested, then performs test and exits" << std::endl;
-    std::cout << "\t-v    indicates that valgrind is being used" << std::endl;
-    std::cout << "\t-n    indicates that output should not be inserted into database" << std::endl;
-    std::cout << "\t-s    indicates submodules are not to be tested" << std::endl;
+    std::cout << "\t-h         prints this help menu and exits" << std::endl;
+    std::cout << "\t-i <value> number of iterations to run the test (default = 1)" << std::endl;
+    std::cout << "\t-t         is followed by name of method to be tested, then performs test and exits" << std::endl;
+    std::cout << "\t-v         indicates that valgrind is being used" << std::endl;
+    std::cout << "\t-n         indicates that output should not be inserted into database" << std::endl;
+    std::cout << "\t-s         indicates submodules are not to be tested" << std::endl;
     //std::cout << "\t--version prints ATL version information\n"<< std::endl;
     std::cout << "Method names to follow t are: Timer, Thread, "
          << "MultiThread, ThreadPool, TaskManager, TSQueue, "
@@ -34,6 +35,7 @@ bool printHelp(void)
  **/
 int main(int argc, char *argv[])
 {
+    int  iterations = 1;
     bool testSubmodules = true;
     bool testAll = true;
     bool valgrind = false;
@@ -56,6 +58,15 @@ int main(int argc, char *argv[])
             unitList.push_back( argv[i]);
         } else if(strcmp(argv[i], "-n") == 0){
             insert = false;
+        } else if( !strcmp(argv[i], "-i" )) {
+           if( argc <= ++i ) {
+              std::cout << "-i option must have a specified value!"<<std::endl;
+              printHelp();
+           }
+           else {
+              iterations = atoi(argv[i]);
+           }
+
         } else if(strcmp(argv[i], "-s") == 0){
             testSubmodules = false;
         } else if(strcmp(argv[i], "-h") == 0){
@@ -69,18 +80,19 @@ int main(int argc, char *argv[])
     }
 
     //run tests
-    std::cout << "Testing AquetiTools..." << std::endl;
-    JsonBox::Value result = atl::testAquetiTools(testSubmodules, printFlag, assertFlag, valgrind, unitList);
-    if(result["pass"] == true){
-        std::cout << "AquetiTools passed successfully!" << std::endl;
-        return 0;
-    }
-    else{
-        std::cout << "AquetiTools failed to pass!" << std::endl;
-        return 1;
+    for( int i = 0; i < iterations; i++ ) {
+       std::cout << "AquetiTools test "<<i+1<<" of "<<iterations<<"..." << std::endl;
+       JsonBox::Value result = atl::testAquetiTools(testSubmodules, printFlag, assertFlag, valgrind, unitList);
+       if(result["pass"] == true){
+           std::cout << "AquetiTools test "<<i+1<<" of "<<iterations<<" passed!" << std::endl;
+       }
+       else{
+           std::cout << "AquetiTools test "<<i+1<<" of "<<iterations<<" failed!" << std::endl;
+           return 1;
+       }
     }
 
-
+/*
     //connect to database and insert JsonValue if "-n" was not used
     if(insert){
         std::cout << "Inserting unit test results in database..." << std::endl;
@@ -97,6 +109,6 @@ int main(int argc, char *argv[])
             return 0;
         }
     }
-
+*/
     return 0;
 }
